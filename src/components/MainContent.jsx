ea86b37abbed729cadec4e80209dc40b9feefd51
mithun20522 <mithun20522@iiitd.ac.in>
2024-04-25 22:37:36 +0530

    all minor issue fixed

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,7 +5,7 @@ const MainContent = () => {
   return (
     <div className=' w-[100%] mx-5 mt-5 bg-white border rounded-xl p-5 space-y-2'>
         <span className='mx-8 mb-5'>Jobs{'('}44{')'}</span>
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col gap-2 overflow-y-auto max-h-[70vh]'>
             <JobDetailsCard percent={92} color={'green'}/>
             <JobDetailsCard percent={87} color={'yellow'}/>
             <JobDetailsCard percent={87} color={'yellow'}/>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ import { SiStarship } from "react-icons/si";
 
 const Sidebar = () => {
   return (
-    <div className="w-[300px] p-5 bg-white border-r-2 min-h-screen flex flex-col justify-between">
+    <div className="w-[300px] p-5 bg-white border-r-2 max-h-screen flex flex-col justify-between">
         <div>
             <ul className="flex flex-col gap-2 text-sm">
                 <div className="flex gap-3 items-center">
@@ -72,7 +72,7 @@ const Sidebar = () => {
             <div className="border px-1 py-2 mb-3 text-slate-800 rounded-lg text-xs text-start">Current Plan: Free</div>
             <div className="flex items-center max-w-fit bg-red-50 border border-yellow-500 justify-between py-3 rounded-xl p-3 cursor-pointer px-1 text-xs">
                 <div className="flex items-center gap-4 justify-between">
-                <SiStarship className="text-yellow-600 ml-3 text-3xl"/>
+                <SiStarship className="text-yellow-600  text-3xl"/>
                 <div>
                     <p className="font-semibold">Upgraded to Pro</p>
                     <p className="text-slate-500">Get all features</p>
